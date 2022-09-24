c757a466a6dfda77e7a4e9f94128ddfcf2495572
Siddharth Raj <siddharthraj34@yahoo.com>
2022-09-24 23:43:44 +0530

    comm

--- ch_7_2d_array/ringRotate.java
+++ ch_7_2d_array/ringRotate.java
@@ -132,10 +132,12 @@ public class ringRotate {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         int m = scn.nextInt();
+        
         int [][] arr = new int[n][m];
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
                 arr[i][j] = scn.nextInt();
+            //    / /dfdsjkfdsf
             }
         }
         int s = scn.nextInt();
